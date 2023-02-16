9c98cf115f176b96e9efa9862baeacd3bbf944c3
Thomas Thiboutot <ththib@l4712-06.info.polymtl.ca>
2023-02-15 20:41:45 -0500

    task: partie 2 completée sauf que la valeur de retour donne une somme étrangement proche; regarder les indices des boucles de contribution(probable source d'erreur)

--- INF2610TP1H23_G02L_04L/part2.c
+++ INF2610TP1H23_G02L_04L/part2.c
@@ -17,17 +17,25 @@
 // question2 calcule la somme des m premiers nombres naturels 1+2+...+m
 
 // nb est le nombre de threads qui vont contribuer au calcul
-#define nb 4
+#define nb 8
 
 //tableau somme est utilisé pour le calcul des sommes patielles par les threads
 long somme[nb];
 
 
 // fonction exécutée par chaque thread créé
-void* contribution(void*p)
+void*contribution(void* p)
 {
-    // TODO
+  // TODO
+  int *no= (int *)p;
+  long s = 0;
+  long start = (*no * m/nb) + 1;
+  long end = (*no+1) * m/nb;
 
+  for(long i = start; i<= end; i++ ) {
+    s+= i;
+  }
+  somme[*no] = s;
   return NULL;
 }
 
@@ -35,6 +43,18 @@ void* contribution(void*p)
 void question2( )
 {
     // TODO
+    pthread_t thread_id;
+    for(int i = 0; i < nb; i++ ) {
+      pthread_create(&thread_id, NULL, contribution, (void *)&i);
+      pthread_join(thread_id, NULL);
+    }
+    long long resultat = 0; 
+    for(int j = 0; j < nb; j++){
+      resultat += somme[j];
+    }
+    printf("%llu \n", resultat);
+    long long multGauss = (m+1)/2;
+    printf("%llu \n",m * multGauss);
     
 }
 
